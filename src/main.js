499608919c73bc48dc449b5f6451fd3c895a04d9
Alexander Groß <agross@therightstuff.de>
2021-10-28 18:51:26 +0200

    Use Vue 3-compatibe Keycloak library

--- src/main.js
+++ src/main.js
@@ -1,53 +1,20 @@
 import { createApp } from 'vue';
-import Keycloak from 'keycloak-js';
+import { vueKeycloak } from "@baloise/vue-keycloak";
 import App from './App.vue';
 
-const keycloakOptions = {
-  url: process.env.VUE_APP_KEYCLOAK_URL,
-  realm: "ldap",
-  clientId: "ldap-test-vue",
-  onLoad: "login-required",
-};
+const app = createApp(App);
 
-const keycloak = Keycloak(keycloakOptions);
+app.use(vueKeycloak, {
+  initOptions: {
+    flow: 'standard', // default
+    checkLoginIframe: false, // default
+    onLoad: 'login-required', // default
+  },
+  config: {
+    url: process.env.VUE_APP_KEYCLOAK_URL,
+    realm: 'ldap',
+    clientId: 'ldap-test-vue'
+  }
+});
 
-keycloak
-  .init({ onLoad: keycloakOptions.onLoad })
-  .then((auth) => {
-    if (!auth) {
-      window.location.reload();
-    } else {
-      console.log("Authenticated");
-
-      createApp(App,
-        { props: { keycloak: keycloak } ,
-      }).mount("#app");
-    }
-
-    // Token Refresh
-    setInterval(() => {
-      keycloak
-        .updateToken(70)
-        .then((refreshed) => {
-          if (refreshed) {
-            console.log("Token refreshed" + refreshed);
-          } else {
-            console.log(
-              "Token not refreshed, valid for " +
-                Math.round(
-                  keycloak.tokenParsed.exp +
-                    keycloak.timeSkew -
-                    new Date().getTime() / 1000
-                ) +
-                " seconds"
-            );
-          }
-        })
-        .catch((err) => {
-          console.log("Failed to refresh token: " + JSON.stringify(err));
-        });
-    }, 6000);
-  })
-  .catch((err) => {
-    console.log("Authentication failed: " + JSON.stringify(err));
-  });
+app.mount("#app");
